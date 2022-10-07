c3cddcdbfe662c9cbdbccb7589e9761dd3e2ad4b
EleeektrIK <Htubcnhfwbz2606>
2022-10-07 19:32:17 +0300

    HomeWork2

--- geek-http-server/src/main/java/ru/geekbrains/ResponseSerializer.java
+++ geek-http-server/src/main/java/ru/geekbrains/ResponseSerializer.java
@@ -2,7 +2,23 @@ package ru.geekbrains;
 
 import ru.geekbrains.domain.HttpResponse;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
 public interface ResponseSerializer {
 
-    String serialize(HttpResponse httpResponse);
+
+    default Reader answerRequest(Path paths) throws IOException {
+
+        return Files.newBufferedReader(paths);
+
+    }
+
+    default Reader dontAnswer(Path path1) throws IOException{
+        return new StringReader("<h1>Ошибочка вышла</h1>");
+    }
 }
